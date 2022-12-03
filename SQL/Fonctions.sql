1caddf4528d932923af598dbe5da63b27977ffb3
Atlonos <58007069+TheCrafteur2015@users.noreply.github.com>
2022-12-03 11:05:29 +0100

    Update Fonctions.sql

--- SQL/Fonctions.sql
+++ SQL/Fonctions.sql
@@ -1,12 +1,20 @@
+/* UPPER, LOWER */
+
 Select	netud, UPPER( nometud ), LOWER( prenometud )
 From	Etudiant;
 
 /*  Cette requête affiche les numéros, les noms en MAJUSCULE et les prénoms en minuscule des étudiants  */
 
+
+/* SUM */
+
 Select	SUM( salaire )
 From	Emploi;
 
 /*  Cette requête calcule le total des salaires de tous les employés  */
+
+
+/* AVG */
 
 Select	AVG( moy )
 From	Note;

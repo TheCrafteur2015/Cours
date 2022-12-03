dba7ffa5d5bd5badbb5ca112e2625ba534451636
Atlonos <58007069+TheCrafteur2015@users.noreply.github.com>
2022-12-03 09:42:10 +0100

    Update Concat.sql

--- SQL/Concat.sql
+++ SQL/Concat.sql
@@ -3,3 +3,8 @@ From	Etudiant;
 
 /*  Cette requête crée une colonne personnaliée constituée des valeurs de la colonne 'netud' et 'nometud'  */
 /*  Résultat : 1=Simon  */
+
+Select	Concat(netud, '=', nometud) As colonne nom
+From	Etudiant;
+
+/*  Requête identique à la précédente mais la nouvelle colonne s'appelle 'nom'  */

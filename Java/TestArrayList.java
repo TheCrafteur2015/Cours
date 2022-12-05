941150d094eb39c50cd002597dc7145671dfda72
Atlonos <58007069+TheCrafteur2015@users.noreply.github.com>
2022-12-05 08:13:34 +0100

    Create TestArrayList.java

---
+++ Java/TestArrayList.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+/**
+ *
+ *@author 
+ *@version
+ *@since
+ */
+public class Main {
+
+	/**
+	 *@param args An array of String
+	 *
+	 */
+	public static void main(String[] args) {
+
+		ArrayList<String> liste = new ArrayList<String>();
+
+		liste.add( "Lundi" );
+		liste.add( "Mardi" );
+		liste.add( "Mercredi" );
+		liste.add( "Jeudi" );
+		liste.add( "Vendredi" );
+		liste.add( "Samedi" );
+		liste.add( "Dimanche" );
+
+		for (String str : liste) {
+			
+			System.out.print( String.format("%$1s,", str) );
+
+		}	
+
+	}
+
+}

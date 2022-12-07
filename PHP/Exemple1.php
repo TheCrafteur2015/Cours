2f15c21c7e8977081d422c511f0bb314b6d855a5
Atlonos <58007069+TheCrafteur2015@users.noreply.github.com>
2022-12-07 12:09:52 +0100

    Create Exemple1.php

---
+++ PHP/Exemple1.php
@@ -0,0 +1,17 @@
+<?php
+
+# Déclaration d'une variable de type entier
+$var = 5;
+
+// Affichage
+echo "Ceci est le nombre $var";
+
+// Affiche le type et la valeur
+var_dump($var);
+
+// Déclaration d'un tableau de type entier
+$arr = array(5, 2, 7);
+
+var_dump($arr);
+
+?>

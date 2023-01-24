8c2d656c78334a60b80a4a678104a4f002e54c03
Atlonos <58007069+TheCrafteur2015@users.noreply.github.com>
2023-01-24 10:31:02 +0100

    Update Fonctions.sql

--- SQL/Fonctions.sql
+++ SQL/Fonctions.sql
@@ -1,22 +1,46 @@
 /* UPPER, LOWER */
 
-Select	netud, UPPER( nometud ), LOWER( prenometud )
-From	Etudiant;
+SELECT	netud, UPPER( nometud ), LOWER( prenometud )
+FROM	  Etudiant;
 
 /*  Cette requête affiche les numéros, les noms en MAJUSCULE et les prénoms en minuscule des étudiants  */
 
 
 /* SUM */
 
-Select	SUM( salaire )
-From	Emploi;
+SELECT	SUM( salaire )
+FROM  	Emploi;
 
 /*  Cette requête calcule le total des salaires de tous les employés  */
 
 
 /* AVG */
 
-Select	AVG( moy )
-From	Note;
+SELECT	AVG( moy )
+FROM  	Note;
 
 /*  Cette requête calcule la moyenne des notes  */
+
+
+/* MAX */
+
+SELECT  MAX( moy )
+FROM    Note;
+
+/* Cette requête retourne la note maximale */
+
+
+/* MIN */
+
+SELECT  MIN( moy )
+FROM    Note;
+
+/* Cette requête renvoie la note minimale */
+
+
+/* COUNT */
+
+SELECT  COUNT( moy )
+FROM    Note;
+
+/* Cette requête renvoie le nombre de notes */
